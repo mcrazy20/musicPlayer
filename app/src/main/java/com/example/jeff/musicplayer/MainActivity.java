92116ff908a5b813be5745b439f9ec2ee6f7307b
mcrazy20 <mcrazy300@yahoo.com>
2014-10-19 19:59:25 -0400

    Fixing small bugs with song class

--- app/src/main/java/com/example/jeff/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/jeff/musicplayer/MainActivity.java
@@ -26,8 +26,6 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
